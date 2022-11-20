c7bf20a699a7babec86f7f945875e3607b5292a4
juslexis <114819487+juslexis@users.noreply.github.com>
2022-11-19 20:14:59 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "my-devops-tf-state-bucket"
     key = "main"
     region = "us-east-2"
-    dynamodb_table = "my-dynamodb-table"
+    dynamodb_table = "my-dynamobd-table"
   }
 }
